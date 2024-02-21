a0d74829f1a480fd615095bf049d711821bbf012
Mahdi <mahdiesscv69@gmail.com>
2024-02-21 10:51:56 +0100

    gg

--- main.py
+++ main.py
@@ -5,12 +5,30 @@ from typing import Optional
 
 from typing import Union
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 import requests
 from bs4 import BeautifulSoup
 app = FastAPI()
 
+origins = [
+    "http://apiget-gv5f.onrender.com",
+    "https://apiget-gv5f.onrender.com",
+    "http://render.com",
+    "https://render.com",
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 
