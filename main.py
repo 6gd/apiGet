b4d2ef7d67ec77324a86c19ac5d31f127c70e7e3
Mahdi <mahdiesscv69@gmail.com>
2024-02-21 11:38:38 +0100

    gg

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ origins = [
     "https://localhost.tiangolo.com",
     "http://localhost",
     "http://localhost:8080",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
 ]
 
 app.add_middleware(
